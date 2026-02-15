621fbbb2b757446dc881f66d9b77ce330eb0f363
mihotaka9116 <mihotaka9116@gmail.com>
2026-02-15 11:44:02 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+<script>
+  const hamburger = document.getElementById('js-hamburger');
+  const nav = document.getElementById('js-nav');
+
+  hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('active');
+    nav.classList.toggle('active');
+  });
+</script>
+
+<script>
+  const hamburger = document.getElementById('js-hamburger');
+  const nav = document.getElementById('js-nav');
+  const navLinks = document.querySelectorAll('#js-nav a');
+
+  // ハンバーガーボタンクリックで開閉
+  hamburger.addEventListener('click', function () {
+    hamburger.classList.toggle('active');
+    nav.classList.toggle('active');
+  });
+
+  // メニュー内のリンクをクリックしたら閉じる
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      hamburger.classList.remove('active');
+      nav.classList.remove('active');
+    });
+  });
+</script>
